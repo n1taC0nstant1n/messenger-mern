b9e56520b74f264ab032c827269b45aef3c44572
Nita Constantin <nita.constantin880722@yahoo.com>
2022-09-27 20:02:35 +0300

    259. User Register complete Back End Part 4

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,3 +1,43 @@
+const formidable = require("formidable");
+const validator = require("validator");
 module.exports.userRegister = (req, res) => {
-  console.log("register is working");
+  const form = formidable();
+  form.parse(req, async (err, fields, files) => {
+    const { username, email, password, confirmPassword } = fields;
+    const { image } = files;
+    const error = [];
+
+    if (!username) {
+      error.push("Please provide your user name!");
+    }
+    if (!email) {
+      error.push("Please provide your email!");
+    }
+    if (email && !validator.isEmail) {
+      error.push("Please provide your valid email!");
+    }
+    if (!password) {
+      error.push("Please provide your password!");
+    }
+    if (!confirmPassword) {
+      error.push("Please provide your confirm password");
+    }
+    if (password && confirmPassword !== confirmPassword) {
+      error.push("Your Password and Confirm Password not same");
+    }
+    if (password && password.length < 6) {
+      error.push("Please provide password must be 6 character");
+    }
+    if (Object.keys(files).length === 0) {
+      error.push("Please provide user image!");
+    }
+    if (error.length > 0) {
+      res.status(400).json({
+        error: {
+          errorMessage: error,
+        },
+      });
+    } else {
+    }
+  }); // end formidable
 };
