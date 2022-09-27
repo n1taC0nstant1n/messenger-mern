bd6564aa03134e0afa36e91cfc949d7c43331716
Nita Constantin <nita.constantin880722@yahoo.com>
2022-09-27 19:33:17 +0300

    258. User Register complete Back End Part 3

---
+++ backend/models/authModel.js
@@ -0,0 +1,26 @@
+const { model, Schema } = require("mongoose");
+
+const registerSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      select: false,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("user", registerSchema);
