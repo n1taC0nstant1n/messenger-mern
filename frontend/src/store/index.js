2cd355a7b6c6afa5c40cb383b646445f2791e42a
Nita Constantin <nita.constantin880722@yahoo.com>
2022-09-29 21:00:03 +0300

    283. Get Friends and Display Part 3

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,9 +2,10 @@ import { createStore, compose, combineReducers, applyMiddleware } from "redux";
 
 import thunkMiddleware from "redux-thunk";
 import { authReducer } from "./reducers/authReducer";
-
+import { messengerReducer } from "./reducers/messengerReducer";
 const rootReducer = combineReducers({
   auth: authReducer,
+  messenger: messengerReducer,
 });
 
 const middleware = [thunkMiddleware];
