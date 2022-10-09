fa71c24df5115a8fdcbaf5758db0d764df72caf3
Nita Constantin <nita.constantin880722@yahoo.com>
2022-10-09 11:15:42 +0300

    332. Real Time Message Seen Unseen Delivered Part 1

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,49 +1,62 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import moment from "moment";
 
 const Message = ({ message, currentFriend, scrollRef, typingMessage }) => {
   const { myInfo } = useSelector((state) => state.auth);
   return (
     <>
       <div className="message-show">
-        {message && message.length > 0
-          ? message.map((m) =>
-              m.senderId === myInfo.id ? (
-                <div className="my-message" ref={scrollRef}>
-                  <div className="image-message">
-                    <div className="my-text">
-                      <div className="message-text">
+        {message && message.length > 0 ? (
+          message.map((m) =>
+            m.senderId === myInfo.id ? (
+              <div className="my-message" ref={scrollRef}>
+                <div className="image-message">
+                  <div className="my-text">
+                    <div className="message-text">
+                      {m.message.text === "" ? (
+                        <img src={`./image/${m.message.image}`} alt="" />
+                      ) : (
+                        m.message.text
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <div className="time">
+                  {moment(m.createdAt).startOf("mini").fromNow()}
+                </div>
+              </div>
+            ) : (
+              <div className="fd-message" ref={scrollRef}>
+                <div className="image-message-time">
+                  <img src={`/image/${currentFriend.image}`} alt="" />
+                  <div className="message-time">
+                    <div className="fd-text">
+                      <p className="message-text">
                         {m.message.text === "" ? (
                           <img src={`./image/${m.message.image}`} alt="" />
                         ) : (
                           m.message.text
                         )}
-                      </div>
+                      </p>
                     </div>
-                  </div>
-                  <div className="time">2 Jan 2022</div>
-                </div>
-              ) : (
-                <div className="fd-message" ref={scrollRef}>
-                  <div className="image-message-time">
-                    <img src={`/image/${currentFriend.image}`} alt="" />
-                    <div className="message-time">
-                      <div className="fd-text">
-                        <p className="message-text">
-                          {m.message.text === "" ? (
-                            <img src={`./image/${m.message.image}`} alt="" />
-                          ) : (
-                            m.message.text
-                          )}
-                        </p>
-                      </div>
-                      <div className="time">3 Jan 2022</div>
+                    <div className="time">
+                      {moment(m.createdAt).startOf("mini").fromNow()}
                     </div>
                   </div>
                 </div>
-              )
+              </div>
             )
-          : ""}
+          )
+        ) : (
+          <div className="friend_connect">
+            <img src={`/image/${currentFriend.image}`} alt="" />
+            <h3>{currentFriend.userName} Connect You</h3>
+            <span>
+              {moment(currentFriend.createdAt).startOf("mini").fromNow()}
+            </span>
+          </div>
+        )}
       </div>
 
       {typingMessage &&
