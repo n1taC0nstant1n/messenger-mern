7a63687077385d5012dbf1de45d32e4bee92810d
Nita Constantin <nita.constantin880722@yahoo.com>
2022-10-09 21:17:05 +0300

    335. Real Time Seen Unseen Delivered Message Part 2

--- frontend/src/components/Friends.jsx
+++ frontend/src/components/Friends.jsx
@@ -23,15 +23,45 @@ const Friends = (props) => {
 
       <div className="friend-name-seen">
         <div className="friend-name">
-          <h4>{fndInfo.userName}</h4>
+          <h4
+            className={
+              msgInfo?.senderId !== myId &&
+              msgInfo?.status !== undefined &&
+              msgInfo.status !== "seen"
+                ? "unseen_message"
+                : ""
+            }
+          >
+            {fndInfo.userName}
+          </h4>
           <div className="msg-time">
             {msgInfo && msgInfo.senderId === myId ? (
               <span>You</span>
             ) : (
-              <span>{fndInfo.userName + " "}</span>
+              <span
+                className={
+                  msgInfo?.senderId !== myId &&
+                  msgInfo?.status !== undefined &&
+                  msgInfo.status !== "seen"
+                    ? "unseen_message"
+                    : ""
+                }
+              >
+                {fndInfo.userName + " "}
+              </span>
             )}
             {msgInfo && msgInfo.message.text ? (
-              <span>{msgInfo.message.text.slice(0, 10)}</span>
+              <span
+                className={
+                  msgInfo?.senderId !== myId &&
+                  msgInfo?.status !== undefined &&
+                  msgInfo.status !== "seen"
+                    ? "unseen_message"
+                    : ""
+                }
+              >
+                {msgInfo.message.text.slice(0, 10)}
+              </span>
             ) : msgInfo && msgInfo.message.image ? (
               <span>Send an image</span>
             ) : (
